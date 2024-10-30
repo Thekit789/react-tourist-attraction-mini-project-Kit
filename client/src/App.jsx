8625f7ca72d87a15a655c0ecdf9355ab0df91384
Kittisak Saengsri <play_boy_kit@hotmail.com>
2024-10-30 18:11:25 +0700

    fix: fix render components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import { ArticleSection } from "./components/ArticleSection";
 import { Header } from "./components/Header";
-import { NavBar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <NavBar />
       <ArticleSection />
     </div>
   );
