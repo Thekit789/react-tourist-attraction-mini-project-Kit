b6d0319d7a97e117eac8e519053c9936e2615f65
Kittisak Saengsri <play_boy_kit@hotmail.com>
2024-10-30 10:57:29 +0700

    feat: added Header Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Header } from "./components/Header";
 import { NavBar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <NavBar />
     </div>
   );
